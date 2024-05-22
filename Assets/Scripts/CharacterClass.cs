73d0626b13d1cbc6f6793fab71febae2b86bd5df
Trần Trung Việt <137788372+trungviet2k1@users.noreply.github.com>
2024-05-23 04:45:58 +0700

    add packages

---
+++ Assets/Scripts/CharacterClass.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterClass : MonoBehaviour
+{
+    public string className;
+    public int health;
+    public int mana;
+    public List<string> abilities;
+
+    void Start()
+    {
+        if (className == "Warrior")
+        {
+            health = 200;
+            mana = 50;
+            abilities.Add("Slash");
+            abilities.Add("Block");
+        }
+        else if (className == "Mage")
+        {
+            health = 100;
+            mana = 200;
+            abilities.Add("Fireball");
+            abilities.Add("Teleport");
+        }
+    }
+
+    public void UseAbility(string abilityName)
+    {
+        if (abilities.Contains(abilityName))
+        {
+            Debug.Log(className + " uses " + abilityName);
+        }
+    }
+}
