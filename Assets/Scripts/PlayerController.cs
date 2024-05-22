32c8fb7ec43a13dd445fb58ade985eddc60cd41f
Trần Trung Việt <137788372+trungviet2k1@users.noreply.github.com>
2024-05-23 05:08:23 +0700

    player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Joystick joystick;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float horizontal = joystick.Horizontal;
+        float vertical = joystick.Vertical;
+
+        animator.SetFloat("Horizontal", horizontal);
+        animator.SetFloat("Vertical", vertical);
+
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, targetAngle, 0);
+
+            transform.Translate(moveSpeed * Time.deltaTime * direction, Space.World);
+        }
+    }
+}
